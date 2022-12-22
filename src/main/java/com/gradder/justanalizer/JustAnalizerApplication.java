30be12517e54a7617aafc31954558525a95cb17a
Gradder <rzhitkov@gmail.com>
2022-12-22 19:30:46 +0100

    Add spring boot

---
+++ src/main/java/com/gradder/justanalizer/JustAnalizerApplication.java
@@ -0,0 +1,13 @@
+package com.gradder.justanalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustAnalizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustAnalizerApplication.class, args);
+	}
+
+}
